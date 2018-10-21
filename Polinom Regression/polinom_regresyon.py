4a823ddba9ba6b377adf272bd25627bc1a658ab2
Ozcan Yureklioglu <ozcanyureklioglu@Ozcan-MacBook-Air.local>
2018-10-21 23:12:27 +0300

    tahmin ve regresyon

---
+++ Polinom Regression/polinom_regresyon.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 20:07:43 2018
+
+@author: ozcanyureklioglu
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#------------------------------------------------------
+
+#verilerin yuklenmesi
+veriler=pd.read_csv("maaslar.csv")
+
+#verilerin frame dilimleme(slice)
+x=veriler.iloc[:,1:2]
+y=veriler.iloc[:,2:]
+
+#frame verilerini numPY dizi dönüşümü
+X=x.values
+Y=y.values
+
+
+#linear regression dogrusal regresyon
+from sklearn.linear_model import LinearRegression
+lin_reg=LinearRegression()
+lin_reg.fit(X,Y)
+
+
+#dogrulsal olmayan regreyson(polinom)/2.dereceden
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg=PolynomialFeatures(degree=2)
+x_poly=poly_reg.fit_transform(X)
+lin_reg2=LinearRegression()
+lin_reg2.fit(x_poly,Y)
+
+
+#dogrusal olmayan regresyon(polinom)/4.dereceden
+poly_reg3=PolynomialFeatures(degree=4)
+x_poly3=poly_reg3.fit_transform(X)
+lin_reg3=LinearRegression()
+lin_reg3.fit(x_poly3,Y)
+
+
+
+
+#gorsellestirme
+plt.scatter(X,Y,color='red')
+plt.plot(x,lin_reg.predict(X),color='blue')
+plt.show()
+
+plt.scatter(X,Y,color='red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='orange')
+plt.show()
+
+plt.scatter(X,Y,color='red')
+plt.plot(X,lin_reg3.predict(poly_reg3.fit_transform(X)),color='orange')
+plt.show()
+
+
+
--- Tahmin(Prediction)-2/ödev/odev2.py
+++ Tahmin(Prediction)-2/ödev/odev2.py
@@ -22,8 +22,6 @@ print(veriler)
 #------------------------------------------------------
 from sklearn.preprocessing import Imputer,LabelEncoder,OneHotEncoder
 
-
-
 #verileri yukleme
 veriler=pd.read_csv("odev_tenis.csv")
 #print(veriler)
@@ -32,9 +30,6 @@ veriler=pd.read_csv("odev_tenis.csv")
 #------------------------------------------------------
 #numarik veri cevirme
 
-
-
-
 #print(veriler2)
 
 veriler2=veriler.apply(LabelEncoder().fit_transform)
@@ -42,27 +37,14 @@ veriler2=veriler.apply(LabelEncoder().fit_transform)
 le=OneHotEncoder(categorical_features="all")
 c=veriler2.iloc[:,:1]
 c=le.fit_transform(c).toarray()
+
+#verilerin birleştirilmesi
 havadurumu=pd.DataFrame(data=c,index=range(14),columns=['o','r','s'])
 sonveriler=pd.concat([havadurumu,veriler.iloc[:,1:3]],axis=1)
 sonveriler2=pd.concat([veriler2.iloc[:,-2:],sonveriler],axis=1)
 
-
-
-
-
-
-
-
-#verilerin birleştirilmesi
-
-
-
 #print(sonuc)
 #---------------------------------------------------------------
-
-
-
-
 
 
 #test egitimi ve bölme
@@ -77,30 +59,46 @@ sc=StandardScaler()
 #X_test=sc.fit_transform(x_test)
 
 from sklearn.linear_model import LinearRegression
-
+#linear model
 regressor=LinearRegression()
 regressor.fit(x_train,y_train)
 y_pred=regressor.predict(x_test)
-#print(y_pred)
+print("ilk tahmin:",y_pred)
 
-#boy tahmin etme regresion'u
-#boy=sonuc.iloc[:,3:4].values
-#sol=sonuc.iloc[:,:3]
-#sag=sonuc.iloc[:,4:]
-#veri=pd.concat([sol,sag],axis=1)
 
-'''x_train,x_test,y_train,y_test=train_test_split(veri,boy,test_size=0.33,random_state=0)
+x_train,x_test,y_train,y_test=train_test_split(sonveriler2.iloc[:,:-1],sonveriler2.iloc[:,-1:],test_size=0.33,random_state=0)
 
 r2=LinearRegression()
 r2.fit(x_train,y_train)
 y_pred=r2.predict(x_test)
 #print(y_pred)
+#ilk p değeri eleme yok
 import statsmodels.formula.api as sm
-X=np.append(arr=np.ones((22,1)).astype(int),values=veri ,axis=1)
-X_l=veri.iloc[:,[0,1,2,3,4,5]].values
-r_ols=sm.OLS(endog=boy,exog=X_l).fit()
+X=np.append(arr=np.ones((14,1)).astype(int),values=sonveriler2.iloc[:,:-1] ,axis=1)
+X_l=sonveriler2.iloc[:,[0,1,2,3,4,5]].values
+r_ols=sm.OLS(endog=sonveriler2.iloc[:,-1:],exog=X_l).fit()
 print(r_ols.summary())
-'''
+#ilk p değeri regression hesaplama,tahmin etme
+x_train,x_test,y_train,y_test=train_test_split(sonveriler2.iloc[:,:-1],sonveriler2.iloc[:,-1:],test_size=0.33,random_state=0)
+
+r2=LinearRegression()
+r2.fit(x_train,y_train)
+y_pred=r2.predict(x_test)
+print("ikinci tahmin: ",y_pred)
+
+#ikinci geri eleme ve regression hesaplama,tahmin etme
+sonveriler2=sonveriler2.iloc[:,1:]
+X=np.append(arr=np.ones((14,1)).astype(int),values=sonveriler2.iloc[:,:-1] ,axis=1)
+X_l=sonveriler2.iloc[:,[0,1,2,3,4]].values
+r_ols=sm.OLS(endog=sonveriler2.iloc[:,-1:],exog=X_l).fit()
+print(r_ols.summary())
+#Elemeden sonraki tahmin 
+x_train,x_test,y_train,y_test=train_test_split(sonveriler2.iloc[:,:-1],sonveriler2.iloc[:,-1:],test_size=0.33,random_state=0)
+
+r2=LinearRegression()
+r2.fit(x_train,y_train)
+y_pred=r2.predict(x_test)
+print("üçüncü tahmin: ",y_pred)
 
 
 
